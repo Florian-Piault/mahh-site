58d194610776840a5eda1ed6bd178a39596d2c29
Florian PIAULT <florian.piault@mail-ecv.fr>
2022-06-22 11:58:55 +0200

    collections

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import Header from "./components/Header.vue";
 import Hero from "./components/Hero.vue";
 import Expo from "./components/Expo.vue";
-import Collections from "./components/Expo.vue";
+import Collections from "./components/Collections.vue";
 </script>
 
 <template>
